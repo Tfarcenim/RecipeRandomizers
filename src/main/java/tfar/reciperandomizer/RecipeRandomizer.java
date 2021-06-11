3026bd157ee1e556a88511e8d4a14c8b380b6682
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2021-06-11 17:39:40 -0400

    refactors

---
+++ src/main/java/tfar/reciperandomizer/RecipeRandomizer.java
@@ -0,0 +1,14 @@
+package tfar.reciperandomizer;
+
+import net.minecraftforge.fml.common.Mod;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(RecipeRandomizer.MODID)
+public class RecipeRandomizer {
+    // Directly reference a log4j logger.
+
+    public static final String MODID = "reciperandomizer";
+
+    public RecipeRandomizer() {
+    }
+}
